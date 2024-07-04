b874704734f7293ae710452923313516179ba9eb
kittyismylife <chss0520@sookmyung.ac.kr>
2024-07-04 12:59:18 +0900

    Refact: 파일 구조 수정

--- accounts/static/scripts/signup.js
+++ accounts/static/scripts/signup.js
@@ -1,17 +1,14 @@
-const password = document.getElementById("password1");
-const confirmPassword = document.getElementById("password2");
-const message = document.getElementById("message");
-const submitButton = document.getElementById("submitButton");
+document
+  .getElementById("signupForm")
+  .addEventListener("submit", function (event) {
+    var password1 = document.getElementById("password1").value;
+    var password2 = document.getElementById("password2").value;
+    var message = document.getElementById("message");
 
-function validatePasswords() {
-  if (password.value === confirmPassword.value && password.value !== "") {
-    message.style.display = "none";
-    submitButton.disabled = false;
-  } else {
-    message.style.display = "block";
-    submitButton.disabled = true;
-  }
-}
-
-password.addEventListener("input", validatePasswords);
-confirmPassword.addEventListener("input", validatePasswords);
+    if (password1 !== password2) {
+      event.preventDefault(); // 폼 제출을 막음
+      message.style.display = "block"; // 에러 메시지 표시
+    } else {
+      message.style.display = "none"; // 에러 메시지 숨김
+    }
+  });
