8637b4ab172a9b234dacc6a57bde45b861a7a808
kittyismylife <chss0520@sookmyung.ac.kr>
2024-07-05 21:22:50 +0900

    Feat: ㅅㅂ은 장난이고 경로 수정

--- user/accounts/static/scripts/loginCheck.js
+++ user/accounts/static/scripts/loginCheck.js
@@ -1,21 +1,18 @@
-const loggedIn = localStorage.getItem("loggedIn");
-const username = localStorage.getItem("username");
+// const loggedIn = localStorage.getItem("loggedIn");
+// const username = localStorage.getItem("username");
 
-if (loggedIn === "true") {
-  document.getElementById("login").style.display = "none";
-  document.getElementById("signup").style.display = "none";
-  document.getElementById("logout").style.display = "block";
-  console.log("로그인 된 사용자:", username);
-} else {
-  document.getElementById("logout").style.display = "none";
-}
+// // if (loggedIn === "true") {
+// //   document.getElementById("login").style.display = "none";
+// //   document.getElementById("signup").style.display = "none";
+// //   document.getElementById("logout").style.display = "block";
+// //   console.log("로그인 된 사용자:", username);
+// // } else {
+// //   document.getElementById("logout").style.display = "none";
+// // }
 
-document.getElementById("logout").addEventListener("click", function () {
-  localStorage.removeItem("loggedIn");
-  console.log("로그아웃 성공:", username);
-<<<<<<< HEAD
-  window.location.href = "/html/mainpage.html";
-=======
-  window.location.href = "{% url 'mainpage' %}";
->>>>>>> b80f800ce6f26dd3fcbada442c3aea7fbf704b77
-});
+// document.getElementById("logout").addEventListener("click", function () {
+//   localStorage.removeItem("loggedIn");
+//   console.log("로그아웃 성공:", username);
+
+//   window.location.href = "{% url 'mainpage' %}";
+// });
